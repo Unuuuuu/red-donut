c120690c973e6ca923cafa98dacdf95d94694711
지윤우 <apple8250@naver.com>
2020-06-29 22:46:54 +0900

    #3 Refactoring Calendar.js & CalendarItem.js

--- src/component/Calendar.js
+++ src/component/Calendar.js
@@ -3,13 +3,19 @@ import "./Calendar.css";
 import CalendarItem from "./CalendarItem";
 
 const Calendar = () => {
+  const [prevMonth, currentMonth, nextMonth] = [new Date().getMonth() - 1, new Date().getMonth(), new Date().getMonth() + 1];
+  const [prevYear, currentYear, nextYear] = [
+    new Date(new Date().getFullYear(), prevMonth).getFullYear(),
+    new Date(new Date().getFullYear(), currentMonth).getFullYear(),
+    new Date(new Date().getFullYear(), nextMonth).getFullYear(),
+  ];
   return (
     <div className="calendar">
-      <CalendarItem when="prev" />
+      <CalendarItem year={prevYear} month={prevMonth} />
       <div className="calendar-border"></div>
-      <CalendarItem when="current" />
+      <CalendarItem year={currentYear} month={currentMonth} />
       <div className="calendar-border"></div>
-      <CalendarItem when="next" />
+      <CalendarItem year={nextYear} month={nextMonth} />
     </div>
   );
 };
--- src/component/CalendarItem.js
+++ src/component/CalendarItem.js
@@ -5,66 +5,30 @@ import { useMainContext } from "../component/Main";
 import { getGroupedTasksByDate } from "../util/taskUtil";
 
 const CalendarItem = (props) => {
-  const [prevMonth, currentMonth, nextMonth] = [new Date().getMonth() - 1, new Date().getMonth(), new Date().getMonth() + 1];
-  const [prevYear, currentYear, nextYear] = [
-    new Date(new Date().getFullYear(), prevMonth).getFullYear(),
-    new Date(new Date().getFullYear(), currentMonth).getFullYear(),
-    new Date(new Date().getFullYear(), nextMonth).getFullYear(),
-  ];
-  const [prevMonthFirstDay, currentMonthFirstDay, nextMonthFirstDay] = [
-    new Date(prevYear, prevMonth, 1).getDay(),
-    new Date(currentYear, currentMonth, 1).getDay(),
-    new Date(nextYear, nextMonth, 1).getDay(),
-  ];
-
-  const handleYear = () => {
-    if (props.when === "prev") return prevYear;
-    else if (props.when === "current") return currentYear;
-    else if (props.when === "next") return nextYear;
-  };
-
-  const handleMonth = () => {
-    if (props.when === "prev") return prevMonth;
-    else if (props.when === "current") return currentMonth;
-    else if (props.when === "next") return nextMonth;
-  };
-
-  const handleAllDays = () => {
-    if (props.when === "prev") return prevAllDays;
-    else if (props.when === "current") return currentAllDays;
-    else if (props.when === "next") return nextAllDays;
-  };
+  const monthFirstDay = new Date(props.year, props.month, 1).getDay();
 
   const createAlldays = (v, i) => {
-    let monthFirstDay;
-    if (props.when === "prev") monthFirstDay = prevMonthFirstDay;
-    else if (props.when === "current") monthFirstDay = currentMonthFirstDay;
-    else if (props.when === "next") monthFirstDay = nextMonthFirstDay;
-
     let date = i - monthFirstDay + 1;
     if (date < 0) date = 0;
-    else if (date > getLastDateFromYearAndMonth(handleYear(), handleMonth())) date = 0;
+    else if (date > getLastDateFromYearAndMonth(props.year, props.month)) date = 0;
     return {
       date: date,
       mode: 0,
     };
   };
 
-  const prevAllDays = Array.from(Array(42), (v, i) => createAlldays(v, i));
-  const currentAllDays = Array.from(Array(42), (v, i) => createAlldays(v, i));
-  const nextAllDays = Array.from(Array(42), (v, i) => createAlldays(v, i));
+  const allDays = Array.from(Array(42), (v, i) => createAlldays(v, i));
 
   const { tasks } = useMainContext();
   const groupedTasksByDate = useMemo(() => getGroupedTasksByDate(tasks), [tasks]);
 
-  const fnct = () => {
-    const month = handleMonth();
-    return groupedTasksByDate.filter((v) => new Date(v[0].time).getMonth() === month);
+  const getDaysByMonth = () => {
+    return groupedTasksByDate.filter((v) => new Date(v[0].time).getMonth() === props.month);
   };
 
-  const daysByMonth = fnct();
+  const daysByMonth = getDaysByMonth();
 
-  const getDateFromMonth = (daysByMonth) => {
+  const getDaysByDate = (daysByMonth) => {
     return daysByMonth.map((days) => {
       const mode = days.reduce((a, c) => a + c.mode, 0);
       return {
@@ -74,10 +38,9 @@ const CalendarItem = (props) => {
     });
   };
 
-  const daysByDate = getDateFromMonth(daysByMonth);
+  const daysByDate = getDaysByDate(daysByMonth);
 
-  const replaceAllDaysByTask = (daysByDate) => {
-    const allDays = handleAllDays();
+  const getAllDaysByTask = (daysByDate) => {
     let arr;
     if (daysByDate.length === 0) return allDays;
     else
@@ -92,13 +55,12 @@ const CalendarItem = (props) => {
     return arr;
   };
 
-  const allDaysByTask = replaceAllDaysByTask(daysByDate);
-  console.log(allDaysByTask);
+  const allDaysByTask = getAllDaysByTask(daysByDate);
 
   return (
     <div className="calendar-item">
       <div className="calendar-date">
-        {handleYear()}. {handleMonth() + 1}.
+        {props.year}. {props.month + 1}.
       </div>
       <div className="calendar-days">
         <div className="calendar-day">일</div>
